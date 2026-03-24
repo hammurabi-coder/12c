64f83fcd878345dce92effd29c0941247d6e7cce
pshap <pshap@example.com>
2026-03-24 19:56:20 -0400

    Fix: Add missing favicon.png and uncomment link

---
+++ docs/_app/immutable/entry/start.BJnaN5Md.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BYdhihUp.js";import{A as t}from"../chunks/BDiKQ8PR.js";export{t as load_css,r as start};
--- docs/_app/immutable/entry/start.DLl9F1qp.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/mIAMVerm.js";import{A as t}from"../chunks/BOp4gMQ1.js";export{t as load_css,r as start};

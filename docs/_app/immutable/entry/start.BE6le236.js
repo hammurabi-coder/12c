ae81735bb1bc7bf32a97833391e35152b023ac91
pshap <pshap@example.com>
2026-03-24 18:48:53 -0400

    Deploy: Permanent .nojekyll fix via static folder

--- docs/_app/immutable/entry/start.BE6le236.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/Ds2b3wrm.js";import{x as t}from"../chunks/DhFNiXDL.js";export{t as load_css,r as start};
---
+++ docs/_app/immutable/entry/start.CAX9FrXJ.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Dcf94I8i.js";import{x as t}from"../chunks/BxGLdWCZ.js";export{t as load_css,r as start};

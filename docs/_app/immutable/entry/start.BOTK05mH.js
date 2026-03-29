5fbbffd74a883a72ca50c016f108684f4d413f5a
hammurabi-coder <hammurabi-coder@users.noreply.github.com>
2026-03-29 09:38:51 -0400

    fix(tiberius): Claudian family gens link -> Gens_Claudia
    The patrician branch of the Claudian family refers to the gens
    Claudia, not the emperor Claudius.

--- docs/_app/immutable/entry/start.BOTK05mH.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CgeZuBMk.js";import{B as t}from"../chunks/bu35r4My.js";export{t as load_css,r as start};
---
+++ docs/_app/immutable/entry/start.CmSL8txL.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DrZ4grjI.js";import{B as t}from"../chunks/_5XMACDa.js";export{t as load_css,r as start};
